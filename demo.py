a615e72ad7393bb903f6b79840255a1ace4a5242
Ayoub Malek <15731839+SuperKogito@users.noreply.github.com>
2018-05-05 23:12:50 +0200

    Commenting and cleaning up the code

--- demo.py
+++ demo.py
@@ -11,28 +11,46 @@ rcParams['figure.figsize'] = 15, 10
 
 
 def get_data(filename):
+    """ 
+    The get_data function reads data from a csv file and returns three data lists that will be later used in the model 
+    generation.
+    """ 
+    
     # Read csv file as panda dataframe
     data = pd.read_csv(filename)
     data.columns = ['Date', 'Open', 'High', 'Low', 'Close', 'Volume', 'MarketCap']
+    
     # Adjast frames
     data['Date'] = pd.to_datetime(data['Date'])
     data['Average'] = data.eval('Open + Close') / 2
+    
     # Adjust diff column
     data['Diff'] = data['Average'] - data['Average'].shift(1)
+    
     # Return data as lists
     return data['Date'].tolist()[1:], data['Average'].tolist()[1:], data['Diff'].tolist()[1:]
 
 
 def predict_price(indices, dates, prices, x):
+    """ 
+    The predict_price function takes in three lists, uses them for the support vector regression model generation, makes a
+    prediction based on a certain amoount of the data and plots the results. This function returns a single value list, that
+    is the prediction.
+    """ 
+    
     # Converting to matrix of n X 1
     print('>>>>> STATUS: MATRIX CONVERSION')
     indices = np.reshape(indices,(len(indices), 1))
+    
     # Defining the support vector regression models
     print('>>>>> STATUS: DEFINING SVR MODEL')
+    
     # Defining the support vector regression models
     svr_rbf = SVR(kernel= 'rbf', C= 1e3, gamma= 0.1)
+    
     # Fitting the data points in the models
     svr_rbf.fit(indices, prices)
+    
     # Plotting the results
     print('>>>>> STATUS: PLOTTING')
     plt.scatter(dates, prices, color= 'black', label= 'Data')
@@ -43,14 +61,25 @@ def predict_price(indices, dates, prices, x):
     plt.title('Support Vector Regression')
     plt.legend()
     plt.show()
-
+    
+    # Making a prediction
     print('>>>>> STATUS: MAKING A PREDICTION')
     return svr_rbf.predict(x)
 
 
 def delete_outlayers(input_list, price_diff):
+    """ 
+    The delete_outlayers function can be used or ditched depending on the user's wish. It takes in two lists and it deletes
+    the values in the first with the same indices of the maximum and minimum of the second list, which is the major forecasting
+    variable. This operations     seeks to delete outlayers and unclassified values that may have a negative effect on the plot 
+    quality.
+    """ 
+    
+    # Get maximum and minimum indices
     max_pos = price_diff.index(max(price_diff))
     min_pos = price_diff.index(min(price_diff))
+    
+    # Remove correspondant values from input list
     input_list.remove(input_list[max_pos])
     input_list.remove(input_list[min_pos])
 
